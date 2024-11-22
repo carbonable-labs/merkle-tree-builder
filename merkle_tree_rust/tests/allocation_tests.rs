242cdd4f04b892e4b8d104aeb093c351775bd731
jaykayudo <codebestia@gmail.com>
2024-11-22 13:24:49 +0100

    (chore) add more tests for allocation

--- merkle_tree_rust/tests/allocation_tests.rs
+++ merkle_tree_rust/tests/allocation_tests.rs
@@ -1,5 +1,9 @@
+#[warn(unused_imports)]
+
 #[cfg(test)]
 mod tests {
+    use std::{hash::{DefaultHasher, Hash, Hasher}, u64};
+
     use merkle_tree::core::allocation::{u64_to_felt, Allocation};
     use starknet::core::types::Felt;
 
@@ -23,6 +27,32 @@ mod tests {
     }
 
     #[test]
+    fn test_allocation_invalid_address() {
+        let allocation = Allocation {
+            address: "0x12939jojdo30".to_string(),
+            amount: 150,
+            timestamp: "0x".to_string(),
+            id: 1
+        };
+        let felts = allocation.to_felts();
+        assert!(felts.is_err());
+        assert_eq!(felts.unwrap_err(), "Invalid address");
+    }
+
+    #[test]
+    fn test_allocation_invalid_timestamp() {
+        let allocation = Allocation {
+            address: "0x1234567890abcdef1234567890abcdef12345678".to_string(),
+            amount: 150,
+            timestamp: "0x38djij".to_string(),
+            id: 1,
+        };
+        let felts = allocation.to_felts();
+        assert!(felts.is_err());
+        assert_eq!(felts.unwrap_err(), "Invalid timestamp");
+    }
+
+    #[test]
     fn test_u64_to_felt_conversion() {
         let value = 150;
         let felt = u64_to_felt(value);
@@ -32,5 +62,41 @@ mod tests {
         // Ensure the serialized bytes of `felt` match the original value.
         let felt_bytes = felt.to_bytes_be();
         assert_eq!(felt_bytes[24..], value_bytes);
+    }
+
+    #[test]
+    fn test_u64_edge_case() {
+        // validation for the minimal edge case
+        let zero_value = 0;
+        let felt = u64_to_felt(zero_value);
+        assert!(matches!(felt, Felt { .. }));
+
+        // validation for max value
+        let max_value = u64::MAX;
+        let felt = u64_to_felt(max_value);
+        assert!(matches!(felt, Felt { .. }));
+    }
+
+    #[test]
+    fn test_allocation_hash() {
+        let allocation = Allocation {
+            address: "0x1234567890abcdef1234567890abcdef12345678".to_string(),
+            amount: 150,
+            timestamp: "0x2".to_string(),
+            id: 1,
+        };
+        let mut state = DefaultHasher::new();
+        allocation.hash(&mut state);
+        let hash = state.finish();
+
+        assert_ne!(hash, 0, "Hash should not be zero");
+        assert_eq!(std::mem::size_of_val(&hash), 8, "Hash should be u64");
+
+        let mut other_state  = DefaultHasher::new();
+        allocation.hash(&mut other_state);
+        let other_hash = other_state.finish();
+
+        assert_eq!(hash, other_hash, "Hash should be the same");
+
     }
 }
